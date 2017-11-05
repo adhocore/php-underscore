5adc61424e058518aa830b520408254b785fba63
Jitendra Adhikari <jiten.adhikary@gmail.com>
2017-11-05 21:27:29 +0700

    refactor: docblock, make arg optional

--- src/functions.php
+++ src/functions.php
@@ -3,7 +3,14 @@
 \class_alias('Ahc\Underscore\Underscore', 'Ahc\Underscore');
 
 if (!\function_exists('underscore')) {
-    function underscore($data)
+    /**
+     * Underscore instantiation helper.
+     *
+     * @param mixed $data
+     *
+     * @return Ahc\Underscore\Underscore
+     */
+    function underscore($data = [])
     {
         return new Ahc\Underscore\Underscore($data);
     }
