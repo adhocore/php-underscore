85a200227208dcd64458a83b9b31032b834c4d21
Jitendra Adhikari <jiten.adhikary@gmail.com>
2017-11-09 22:19:18 +0700

    fix: memoize() and delay(), remove bind(), cleanup throttle()

--- src/Underscore.php
+++ src/Underscore.php
@@ -4,39 +4,62 @@ namespace Ahc\Underscore;
 
 final class Underscore extends UnderscoreArray
 {
-    public function bind(\Closure $fn, $ctx)
-    {
-        \Closure::bind($fn, \is_object($ctx) ? $ctx : null);
-    }
-
+    /**
+     * Returns a callable which when invoked caches the result for given arguments
+     * and reuses that result in subsequent calls.
+     *
+     * @param callable $fn The main callback.
+     *
+     * @return mixed
+     */
     public function memoize(callable $fn)
     {
         static $memo = [];
 
-        $hash = \md5(\json_encode(\array_slice(\func_get_args(), 1)));
+        return function () use (&$memo, $fn) {
+            $hash = \md5(\json_encode($args = \func_get_args()));
 
-        if (isset($memo[$hash])) {
-            return $memo[$hash];
-        }
+            if (isset($memo[$hash])) {
+                return $memo[$hash];
+            }
 
-        return $memo[$hash] = \call_user_func_array($fn, $args);
+            return $memo[$hash] = \call_user_func_array($fn, $args);
+        };
     }
 
+    /**
+     * Cache the result of callback for given arguments and reuse that in subsequent call.
+     *
+     * @param callable $fn   The main callback.
+     * @param int      $wait The time to wait in millisec.
+     *
+     * @return mixed
+     */
     public function delay(callable $fn, $wait)
     {
-        usleep(1000 * $wait);
+        return function () use ($fn, $wait) {
+            \usleep(1000 * $wait);
 
-        return \call_user_func_array($fn, \array_slice(\func_get_args(), 2));
+            return \call_user_func_array($fn, \func_get_args());
+        };
     }
 
-    public function throttle($fn, $wait)
+    /**
+     * Returns a callable that wraps given callable which can be only invoked
+     * at most once per given $wait threshold.
+     *
+     * @param callable $fn   The main callback.
+     * @param int      $wait The callback will only be triggered at most once within this period.
+     *
+     * @return mixed The return set of callback if runnable else the previous cache.
+     */
+    public function throttle(callable $fn, $wait)
     {
-        $previous = 0;
-        $result   = null;
+        static $previous = 0;
+        static $result   = null;
 
         return function () use ($fn, &$previous, &$result, &$wait) {
-            $now  = $this->now();
-            $args = \func_get_args();
+            $now = $this->now();
 
             if (!$previous) {
                 $previous = $now;
@@ -46,7 +69,7 @@ final class Underscore extends UnderscoreArray
 
             if ($remaining <= 0 || $remaining > $wait) {
                 $previous = $now;
-                $result   = \call_user_func_array($fn, $args);
+                $result   = \call_user_func_array($fn, \func_get_args());
             }
 
             return $result;
