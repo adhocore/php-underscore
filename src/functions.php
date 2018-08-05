ef89d9ae73ccfcdbbfe0758849570d08d1a8ca8c
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-08-05 09:34:57 +0000

    Apply fixes from StyleCI

--- src/functions.php
+++ src/functions.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the PHP-UNDERSCORE package.
+ *
+ * (c) Jitendra Adhikari <jiten.adhikary@gmail.com>
+ *     <https://github.com/adhocore>
+ *
+ * Licensed under MIT license.
+ */
+
 use Ahc\Underscore\Underscore;
 
 \class_alias(Underscore::class, 'Ahc\\Underscore');
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the PHP-UNDERSCORE package.
+ *
+ * (c) Jitendra Adhikari <jiten.adhikary@gmail.com>
+ *     <https://github.com/adhocore>
+ *
+ * Licensed under MIT license.
+ */
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 if (class_exists('\PHPUnit_Framework_Error_Notice')) {
