6ba1916efce13e17f23328baf9dd2499b020751e
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-08-19 11:27:15 +0700

    feat(hom): support higher order messaging

---
+++ src/HigherOrderMessage.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the PHP-UNDERSCORE package.
+ *
+ * (c) Jitendra Adhikari <jiten.adhikary@gmail.com>
+ *     <https://github.com/adhocore>
+ *
+ * Licensed under MIT license.
+ */
+
+namespace Ahc\Underscore;
+
+/**
+ * Source: Laravel HigherOrderProxy
+ *
+ * @link https://github.com/laravel/framework/pull/16267
+ */
+class HigherOrderMessage
+{
+    protected $underscore;
+    protected $method;
+
+    public function __construct(UnderscoreBase $underscore, $method)
+    {
+        $this->underscore = $underscore;
+        $this->method     = $method;
+    }
+
+    public function __call($method, $args)
+    {
+        $this->underscore->{$this->method}(function ($item) use ($method, $args) {
+            return \call_user_func_array([$item, $method], $args);
+        });
+    }
+
+    public function __get($prop)
+    {
+        $this->underscore->{$this->method}(function ($item) use ($prop) {
+            $props = \array_column([$item], $prop);
+
+            return $props ? $props[0] : null;
+        });
+    }
+}
